6b068ac22f57660b3c96fd8c25535db1c0236d40
Antonio Valentino <antonio.valentino@tiscali.it>
2016-12-22 21:38:25 +0100

    Migrations

--- areweblic/app.py
+++ areweblic/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap as bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import UploadSet, configure_uploads
+from flask_migrate import Migrate
 
 from . import config
 
@@ -17,6 +18,7 @@ bootstrap(app)
 
 # sqlalchemy
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # flask-uploads
 request_uploader = UploadSet('requests', config.REQUEST_EXTENSIONS)
