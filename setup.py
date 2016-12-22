4af86ffcf3f7a355c821db1a90a12f9aafdad0d1
Antonio Valentino <antonio.valentino@tiscali.it>
2016-12-22 21:36:16 +0100

    setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='areweblic',
+    packages=['areweblic'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'flask-bootstrap',
+        # 'flask-login',
+        'flask-sqlalchemy',
+        'flask-uploads',
+        # 'flask-user',
+    ],
+)
