9c7543a1df3f1fc42ef9bd0740f79f8f47d8ea94
Antonio Valentino <antonio.valentino@tiscali.it>
2016-12-27 11:37:35 +0100

    Better table names for users adn roles

--- areweblic/models.py
+++ areweblic/models.py
@@ -12,11 +12,13 @@ db = SQLAlchemy()
 # Secutiry
 roles_users = db.Table(
     'roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+    db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('roles.id')))
 
 
 class Role(db.Model, RoleMixin):
+    __tablename__ = 'roles'
+
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
@@ -27,6 +29,8 @@ class Role(db.Model, RoleMixin):
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     #first_name = db.Column(db.String(255))
     #last_name = db.Column(db.String(255))
