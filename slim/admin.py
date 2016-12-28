dc3a8e8cff1e1312e552e60dbe464b1bdb39b588
Antonio Valentino <antonio.valentino@tiscali.it>
2016-12-29 00:28:16 +0100

    Small refinements to admin views

--- slim/admin.py
+++ slim/admin.py
@@ -18,7 +18,7 @@ class AdminIndexView(_AdminIndexView):
 
 class ModelView(sqla.ModelView):
     column_display_pk = True
-    column_display_all_relations = True
+    #column_display_all_relations = True
 
     def is_accessible(self):
         if current_user.has_role('admin'):
@@ -36,6 +36,47 @@ class ModelView(sqla.ModelView):
                 return redirect(url_for('security.login', next=request.url))
 
 
+class UserModelView(ModelView):
+    column_formatters = dict(password=lambda v, c, m, p: '*' * 6)
+    column_display_all_relations = True
+    column_list = (
+        'id',
+        'email',
+        'password',
+        'active',
+        'last_login_at',
+        'current_login_at',
+        'last_login_ip',
+        'current_login_ip',
+        'login_count',
+        'roles',
+    )
+
+
+class PurchaseModelView(ModelView):
+    column_auto_select_related = True
+    column_display_all_relations = True
+    column_list = ('id', 'user_id', 'product_id', 'quantity')
+
+
+class LicenseModelView(ModelView):
+    column_auto_select_related = True
+    column_display_all_relations = True
+    column_list = (
+        'id',
+        'user_id',
+        'product_id',
+        'description',
+        'request',
+        'request_date',
+        'license',
+    )
+    column_formatters = dict(
+        request=lambda v, c, m, p: m.request[:10] + '...',
+        licnse=lambda v, c, m, p: m.license[:10] + '...',
+    )
+
+
 admin = Admin(
     name='slim',
     index_view=AdminIndexView(name='Admin'),
@@ -44,10 +85,10 @@ admin = Admin(
 )
 
 admin.add_view(ModelView(models.Role, models.db.session))
-admin.add_view(ModelView(models.User, models.db.session))
+admin.add_view(UserModelView(models.User, models.db.session))
 admin.add_view(ModelView(models.Product, models.db.session))
-admin.add_view(ModelView(models.Purchase, models.db.session))
-admin.add_view(ModelView(models.License, models.db.session))
+admin.add_view(PurchaseModelView(models.Purchase, models.db.session))
+admin.add_view(LicenseModelView(models.License, models.db.session))
 
 admin.add_link(MenuLink('Home', endpoint='index'))
 admin.add_link(MenuLink('Logout', endpoint='security.logout'))
