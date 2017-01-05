fb2ef94f7cdf868a9093bb9d03429cb9ef2f1b39
Antonio Valentino <antonio.valentino@tiscali.it>
2017-01-05 22:28:48 +0100

    Formatting

--- slim/config.py
+++ slim/config.py
@@ -7,7 +7,7 @@ import sys
 
 from . import utils
 
-# slim
+# --- slim -------------------------------------------------------------------
 SLIM_APPNAME = 'SLiM'
 SLIM_WELCOME_MESSAGE = 'Simple License Management system (SLiM)'
 SLIM_COPYRIGHT_YEAR = 2017
@@ -28,23 +28,28 @@ SLIM_LICENSE_GENERATOR_CMD = [
 SLIM_FILE_LOGGING_LEVEL = None   # no logging on file
 SLIM_FILE_LOGGING_FORMAT = (
     '%(asctime)s - %(name)s - %(levelname)s: %(message)s')
-SLIM_FILE_LOGGING_MAXBYTES = 10 * 1024**2     # 10 MB
+SLIM_FILE_LOGGING_MAXBYTES = 10 * 1024**2   # 10 MB
 SLIM_FILE_LOGGING_BACKUPCOUNT = 5
 
 
-# flask
+# --- flask ------------------------------------------------------------------
 # DEBUG = True
+# TESTING = False
 SECRET_KEY = 'something hard to guess'      # XXX: change this
 # SERVER_NAME = 'slim.local'
 MAX_CONTENT_LENGTH = 4 * 1024
 
 
-# sqlalchemy
+# --- bootstrap --------------------------------------------------------------
+# BOOTSTRAP_SERVE_LOCAL = False
+
+
+# --- sqlalchemy -------------------------------------------------------------
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 
-# security
+# --- security ---------------------------------------------------------------
 SECURITY_PASSWORD_HASH = 'sha512_crypt'
 SECURITY_PASSWORD_SALT = 'slimsalt'         # XXX: change this
 SECURITY_CONFIRMABLE = False
@@ -56,6 +61,6 @@ SECURITY_CHANGEABLE = True
 SECURITY_SEND_PASSWORD_CHANGE_EMAIL = False
 
 
-# upload
+# --- upload -----------------------------------------------------------------
 UPLOADED_REQUESTS_DEST = os.path.join('instance', 'uploads')
 # UPLOADED_REQUESTS_URL = 'http://localhost:5001/uploads'
